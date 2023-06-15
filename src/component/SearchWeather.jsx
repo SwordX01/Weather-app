c2726266426b0f8fb6ab8a5a5aa9a0c9424223a2
sif eddine <bsifeddine23@gmail.com>
2023-06-15 16:23:41 +0100

    first commit

---
+++ src/component/SearchWeather.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Searchweather() {
+    const [search, setSearch] = useState('london');
+    const [data, setData] = useState([]);
+    const [input, setInput] = useState('');
+
+
+    useEffect(() => {
+        let componentMounted = true;
+        const fetchWeather = async () => {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=9ebfa5faed964d8f7aadd1ea27d3d1fc`);
+            const data = await response.json();
+            if (componentMounted) {
+                setData(data);
+            }
+        };
+        fetchWeather();
+        return () => {
+            componentMounted = false;
+        };
+    }, [search]);
+    /*useEffect(() => {
+        if (search) {
+            fetch(
+                `http://api.timezonedb.com/v2.1/get-time-zone?key=YOUR_API_KEY&format=json&by=city&city=${search}`
+            )
+                .then((response) => response.json())
+                .then((data) => {
+                    setTime(data.formatted);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [search]);*/
+
+    let emoji = null
+    if (typeof data.main != "undefined") {
+        if (data.weather[0].main == 'Clouds') {
+            emoji = "fa-cloud"
+        } else if (data.weather[0].main == "Thunderstorm") {
+            emoji = "fa-bolt"
+        } else if (data.weather[0].main == "Drizzle") {
+            emoji = "fa-cloud-rain"
+        } else if (data.weather[0].main == "Rain") {
+            emoji = "fa-cloud-shower-heavy"
+        } else if (data.weather[0].main == "Snow") {
+            emoji = "fa-snow-flake"
+        } else {
+            emoji = "fa-smog"
+        }
+    } else {
+        return (
+            <div>...Loading</div>
+        )
+    }
+    let temp = (data.main.temp - 273.15).toFixed(2);
+    let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+    let temp_max = (data.main.temp_max - 273.15).toFixed(2);
+
+    //date :
+    let d = new Date();
+    let date = d.getDate();
+    let year = d.getFullYear();
+    let month = d.toLocaleString('default', { month: 'long' })
+    let day = d.toLocaleString('default', { weekday: 'long' })
+    //time :
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearch(input);
+    }
+
+
+    return (
+
+        <div>
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class='col-md-4'>
+                        <div class="card text-white text-center border-0" >
+                            <img src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`}
+                                class="card-img"
+                                alt="..." />
+                            <div className="card-img-overlay ">
+                                <form onSubmit={handleSubmit}>
+                                    <div class="input-group mb-4 w-75 mx-auto">
+                                        <input type="text"
+                                            class="form-control"
+                                            placeholder="Search City"
+                                            aria-label="Search City"
+                                            aria-describedby="basic-addon2"
+                                            name='search'
+                                            value={input}
+                                            onChange={(e) => setInput(e.target.value)}
+                                            required />
+                                        <button type='submit' class="input-group-text" id="basic-addon2">
+                                            <i className='fas fa-search'></i>
+                                        </button>
+                                    </div>
+                                </form>
+                                <div className="bg-dark bg-opacity-50 py-3">
+                                    <h2 class="card-title">{data.name}</h2>
+                                    <p class="card-text lead">
+                                        {day}, {month} {date} ,{year}
+
+                                    </p>
+                                    <hr />
+                                    <i className={`fas ${emoji} fa-4x`}></i>
+                                    <h1 className='fw-bolder mb-5'>{temp} &deg;C</h1>
+                                    <p className='lead fw-bolder mb-0'>{data.weather[0].main}</p>
+                                    <p className='lead'> {temp_max}&deg;C | {temp_min}&deg;C</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
